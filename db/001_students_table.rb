88e4e2e914dca991ac19b8d00072256c028eed79
Jordan Kline <jordan.kline@gmail.com>
2018-03-01 19:29:11 +0000

    automatically backed up by learn

--- db/001_students_table.rb
+++ db/001_students_table.rb
@@ -1,10 +1,8 @@
 class CreateStudents < ActiveRecord::Migration
   def change
-    create_table :posts do |t|
-      t.string :title
-      t.text :description
- 
-      t.timestamps null: false
+    create_table :students do |s|
+      s.string :first_name
+      s.string :last_name
     end
   end
 end
